4f724488d7f87c93b55aac487d1cbd5aeb916806
Aaron Peikert <aaron.peikert@posteo.de>
2018-08-16 12:33:13 +0200

    remove conversion of binary predictors fix #3

--- scripts/classification/01load.R
+++ scripts/classification/01load.R
@@ -3,13 +3,7 @@ library(here)
 library(haven)
 
 #----load----
-is_binary <- function(x, enforce_numeric = TRUE){
-  if(enforce_numeric&(!is.numeric(x)))return(FALSE)
-  else{
-    length(unique(x))==2
-  }
-}
 data <- read_sas(here("data", "classification", "var_ap.sas7bdat")) %>% 
-  mutate(family = ifelse(family == "", NA, family) %>% as.factor(),
-         ID = as.character(ID)) %>% 
-  mutate_if(is_binary, as.factor)
+  mutate(family = ifelse(family == "", NA, family) %>% factor(),
+         ID = as.character(ID),
+         group = as.factor(group))
