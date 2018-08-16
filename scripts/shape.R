f8ee9e8d35406639bbb4e0019e3856f387cce068
Aaron Peikert <aaron.peikert@posteo.de>
2018-08-16 10:32:27 +0200

    resolves #2

---
+++ scripts/shape.R
@@ -0,0 +1,30 @@
+library(here)
+library(fs)
+library(haven)
+source(here("scripts", "load.R"))
+#----rect----
+rect <- logfiles %>%
+  mutate(parsed = map2(parsed, ocasion, ~mutate(.x, occasion = .y))) %>%
+  pull(parsed) %>%
+  bind_rows() %>%
+  mutate(occasion = as.factor(occasion),
+         accurate = ifelse(time_passed < 0, NA, accurate))
+
+#----retest----
+retest <- rect %>%
+  group_by(subject, occasion) %>%
+  summarise(acc = mean(accurate, na.rm = TRUE),time = mean(time_passed, na.rm = TRUE)) %>% 
+  unite(acc_time, time, acc) %>% 
+  spread(occasion, acc_time) %>% 
+  separate(`1`, c("time1", "acc1"), sep = "_") %>% 
+  separate(`2`, c("time2", "acc2"), sep = "_") %>% 
+  mutate_at(vars(time1:acc2), as.numeric) %>% 
+  select(-`3`)
+
+#----write----
+dir_create(here("out"))
+write_csv(rect, here("out", "long.csv"))
+write_sas(rect, here("out", "long.sas7bdat"))
+
+write_csv(retest, here("out", "wide.csv"))
+write_sas(retest, here("out", "wide.sas7bdat"))
