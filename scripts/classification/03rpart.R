adff5b52745f50c03c1bbbee56975084d125031d
Aaron Peikert <aaron.peikert@posteo.de>
2018-08-16 17:41:31 +0200

    introduce hyperparamter optimization, resolve #8

--- scripts/classification/03rpart.R
+++ scripts/classification/03rpart.R
@@ -1,6 +1,7 @@
 library(rpart)
 library(hmeasure)
 library(here)
+library(rpart.plot)
 source(here("scripts", "classification", "02preprocess.R"))
 
 #----rpart-loocv----
@@ -14,7 +15,13 @@ fit_rpart <- function(recipe, ...){
         method = "class",
         ...)
 }
-data_cv <- mutate(data_cv, rpart_models = map(recipes, fit_rpart))
+hypermat <- as.data.frame(expand.grid(minsplit = 1:20, minbucket = 1:10))
+
+data_cv <- data_cv %>%
+  mutate(hypermat = list(hypermat)) %>% 
+  unnest(hypermat, .preserve = c(splits, recipes))
+
+data_cv <- mutate(data_cv, rpart_models = pmap(list(recipe = recipes, minsplit = minsplit, minbucket = minbucket), fit_rpart))
 
 pred_rpart <- function(split, recipe, model, ...) {
   mod_data <- bake(
@@ -30,9 +37,39 @@ pred_rpart <- function(split, recipe, model, ...) {
 data_cv <- mutate(data_cv,
                   predicted_rpart = pmap(list(splits, recipes, rpart_models),
                                           pred_rpart))
+data_cv %>%
+  group_by(minsplit, minbucket) %>%
+  nest() %>%
+  pull("data") %>%
+  map("predicted_rpart") %>%
+  map(bind_rows) %>%
+  map(~HMeasure(recode(.x$true, `2` = 0, `1` = 1), .x$predicted)) %>%
+  map("metrics") %>%
+  map_dbl("H")
 
-#----rpart-full----
+data_cv_metrics <- data_cv %>%
+  group_by(minsplit, minbucket) %>%
+  summarise(predicted = predicted_rpart %>%
+              bind_rows() %>%
+              list()) %>% 
+  mutate(metrics = map(predicted,
+                 ~HMeasure(recode(.x$true, `2` = 0, `1` = 1), .x$predicted)) %>%
+           map("metrics")) %>% 
+  unnest(metrics)
+best_hyper_h <- data_cv_metrics %>% filter(H == max(H))
+best_hyper_h <- best_hyper_h[1, c("minsplit", "minbucket")]
+
+best_hyper_f <- data_cv_metrics %>% filter(F == max(F))
+best_hyper_f <- best_hyper_h[1, c("minsplit", "minbucket")]
+
+#----rpart-full-h----
 recipe %>%
   prep(data, retain = TRUE) %>%
-  fit_rpart()
+  fit_rpart(model = TRUE, minsplit = best_hyper_h$minsplit, minbucket = best_hyper_h$minbucket) %>% 
+  rpart.plot::rpart.plot()
 
+#----rpart-full-f----
+recipe %>%
+  prep(data, retain = TRUE) %>%
+  fit_rpart(model = TRUE, minsplit = best_hyper_f$minsplit, minbucket = best_hyper_f$minbucket) %>% 
+  rpart.plot::rpart.plot()
